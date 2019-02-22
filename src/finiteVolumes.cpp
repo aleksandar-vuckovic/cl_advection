b633e25c11ceca2120de7022cf8ffe6f1133a834
Aleksandar Vučković <aleksandar.vuckovic@stud.tu-darmstadt.de>
2019-02-22 12:21:24 +0100

    Major restructuring of source, started implementing initDroplet

--- src/finiteVolumes.cpp
+++ src/finiteVolumes.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
-#include <array>
-#include <vector>
 #include <ctime>
 #include <sstream>
-
-#define _USE_MATH_DEFINES
-#include <cmath>
-
-std::array<double, 2> operator+ (std::array<double, 2> vecA, std::array<double, 2> vecB){
-  return { vecA[0] + vecB[0], vecA[1] + vecB[1]};
-}
-
-std::array<double, 2> operator*(double a, const std::array<double, 2>& vec) {
-  return { a*vec[0], a*vec[1]};
-}
-
-double operator* (const std::array<double, 2>& vecA, const std::array<double, 2>& vecB) {
-  return { vecA[0]*vecB[0] + vecA[1]*vecB[1]};
-}
-
+#include "vecMath2D.h"
 
 std::array<double, 2> shearField(double x, double y) {
-  return {-sin(M_PI*x)*cos(M_PI*y),
-	            cos(M_PI*x)*sin(M_PI*y)};
+  return {-sin(M_PI*x)*cos(M_PI*y), cos(M_PI*x)*sin(M_PI*y)};
 }
 
-void printField(const std::vector <std::vector<double>>& Phi) {
+void printField(const std::vector <std::vector<double>>& Phi, double epsilon) {
   std::ostringstream strs;
   for (int i = 0; i < Phi.size(); i++) {
     for (int j = 0; j < Phi[0].size(); j++) {
-      strs << Phi[i][j];
-      strs << "    ";
+      if (Phi[i][j] < epsilon) {
+	strs << "x";
+	strs << "  ";
+      } else {
+	strs << ".";
+	strs << "  ";
+      }
     }
-    strs << std::endl;
+    strs << std::endl << std::endl;
+    std::cout << strs.str();
   }
-  std::cout << strs.str();
 }
-
+  
 double sumField(const std::vector <std::vector<double>>& Phi) {
   double temp = 0;
   for (int i = 0; i < Phi.size(); i++) {
@@ -47,7 +34,24 @@ double sumField(const std::vector <std::vector<double>>& Phi) {
   return temp;
 }
 
-std::vector <std::vector<double>>& calculateNextTimestep(std::vector <std::vector<double>>& Phi, double dt, double dx) {
+std::vector <std::vector<double>>& initDroplet(std::vector <std::vector<double>>& Phi, std::array<double, 2> center, double radius, double dx, double epsilon) {
+  for (int i = 0; i < Phi.size(); i++) {
+    for (int j = 0; j < Phi[0].size(); j++) {
+      std::array<int, 2> temp = {i, j};
+      std::cout << "test";
+      if (abs(center - temp*dx) < epsilon) {
+	Phi[i][j] = 0.0;
+      } else if (abs(center - temp*dx) > 0) {
+	Phi[i][j] = -1.0;
+      } else {
+	Phi[i][j] = 1.0;
+      }
+    }
+  }
+  return Phi;
+}
+
+std::vector <std::vector<double>>& calculateNextTimestep(std::vector <std::vector<double>>& Phi, double dt, double dx, auto field) {
   const std::array<double, 2> upNormal = {0,1};
   const std::array<double, 2> downNormal = {0,-1};
   const std::array<double, 2> leftNormal = {-1, 0};
@@ -66,23 +70,23 @@ std::vector <std::vector<double>>& calculateNextTimestep(std::vector <std::vecto
 	for (int k = 1; k <= integSteps; k++) {
 	  switch(dir) {
 	  case 0:
-	    temp1 = Phi[x][y]*shearField(x*dx + (k-1)*h, (y+1)*dx)*upNormal;
-	    temp2 = Phi[x][y]*shearField(x*dx + k*h, (y+1)*dx)*upNormal;
+	    temp1 = Phi[x][y]*field(x*dx + (k-1)*h, (y+1)*dx)*upNormal;
+	    temp2 = Phi[x][y]*field(x*dx + k*h, (y+1)*dx)*upNormal;
 	    flux += h/2*(temp1 + temp2);
 	    break;
 	  case 1:
-	    temp1 = Phi[x][y]*shearField(x*dx + (k-1)*h, y*dx)*downNormal;
-	    temp2 = Phi[x][y]*shearField(x*dx + k*h, y*dx)*downNormal;
+	    temp1 = Phi[x][y]*field(x*dx + (k-1)*h, y*dx)*downNormal;
+	    temp2 = Phi[x][y]*field(x*dx + k*h, y*dx)*downNormal;
 	    flux += h/2*(temp1 + temp2);
 	    break;
 	  case 2:
-	    temp1 = Phi[x][y]*shearField(x*dx, y*dx + (k-1)*h)*leftNormal;
-	    temp2 = Phi[x][y]*shearField(x*dx, y*dx +  k*h)*leftNormal;
+	    temp1 = Phi[x][y]*field(x*dx, y*dx + (k-1)*h)*leftNormal;
+	    temp2 = Phi[x][y]*field(x*dx, y*dx +  k*h)*leftNormal;
 	    flux += h/2*(temp1 + temp2);
 	    break;
 	  case 3:
-	    temp1 = Phi[x][y]*shearField((x+1)*dx, y*dx + (k-1)*h)*rightNormal;
-	    temp2 = Phi[x][y]*shearField((x+1)*dx, y*dx +  k*h)*rightNormal;
+	    temp1 = Phi[x][y]*field((x+1)*dx, y*dx + (k-1)*h)*rightNormal;
+	    temp2 = Phi[x][y]*field((x+1)*dx, y*dx +  k*h)*rightNormal;
 	    flux += h/2*(temp1 + temp2);
 	    break;
 	  }
@@ -102,30 +106,40 @@ int main() {
 	double lenY = 1.0;
 	double dx = lenX/numX;
 
-	double time = 10.0;
-	int timesteps = 1000;
+	double time = 0.1;
+	int timesteps = 10;
 	double dt = time/timesteps;
 
 	auto field = shearField;
+	//Initialize empty field
 	std::vector< std::vector<double> > Phi(numX);
+	for (int i = 0; i < numX; i++) {
+	  std::vector<double> temp(numY);
+	  Phi[i] = temp;
+	}
+	std::array<double, 2> center = {0.7, 0.5};
+	double radius = 0.2;
 
 	//Create random field Phi for testing
-	srand(std::time(NULL));
-	for (int i = 0; i < numX; i++) {
-	  std::vector<double> tempY(numY);
-	  for (int j = 0; j < numY; j++) {
-	    tempY[j] = (rand()%10)/10.0;
-	  }
-	  Phi[i] = tempY;
-	}
-	//printField(Phi);
-	std::cout << std::endl << sumField(Phi) << std::endl;
+	//	srand(std::time(NULL));
+	//for (int i = 0; i < numX; i++) {
+	//std::vector<double> tempY(numY);
+	//for (int j = 0; j < numY; j++) {
+	// tempY[j] = (rand()%100)/100.0;
+	//}
+	//Phi[i] = tempY;
+	//	}
+
+        Phi = initDroplet(Phi, center, radius, dx, 0.05);
+	
+	std::cout << "Sum of Phi at start " << sumField(Phi) << std::endl;
 	for (int i = 0; i < timesteps; i++)  {
-	  Phi = calculateNextTimestep(Phi, dt, dx);
+	  //printField(Phi, 0.01);
+	  Phi = calculateNextTimestep(Phi, dt, dx, field);
 	}
 	//printField(Phi);
 	std::cout << std::endl;
-	std::cout << sumField(Phi) << std::endl;
+	std::cout << "Sum of Phi at end " << sumField(Phi) << std::endl;
 
 	return 0;
 }
