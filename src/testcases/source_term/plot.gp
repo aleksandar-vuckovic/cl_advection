669ccfbb16c79fbdfef1b24d137b410221d7ea28
Mathis Fricke <fricke@mma.tu-darmstadt.de>
2022-05-30 13:40:56 +0200

    Introduced "source_term/" testcase based on testcase_tool.

--- src/testcases/source_term/plot.gp
+++ src/testcases/source_term/plot.gp
@@ -1,8 +1,5 @@
 set datafile separator ","
 set term pdf
-
-
-
 set output "results_angle.pdf"
 set key above
 set xlabel "time"
@@ -13,13 +10,11 @@ set style line 1 \
     linetype 1 linewidth 1 \
     pointtype 2 pointsize 0.5
 
-plot "source_on/contactAngle.csv" using 1:2 title "Source On" with line,\
-     "source_off/contactAngle.csv" using 1:2 title "Source Off" with line,\
-     "source_on/contactAngle.csv" using 1:3 title "Reference" with line
+plot "contactAngle.csv" using 1:2 every 10 title "LevelSet" with points pointsize 0.5,\
+     "contactAngle.csv" using 1:3 every 10 title "Reference" with line
 
 set output "results_position.pdf"
 set ylabel "Position"
 
-plot "source_on/position.csv" using 1:2 title "Source On" with line,\
-     "source_off/position.csv" using 1:2 title "Source Off" with line,\
-     "source_on/position.csv" using 1:3 title "Reference" with line
+plot "position.csv" using 1:2 every 10 title "LevelSet" with points pointsize 0.5,\
+     "position.csv" using 1:3 every 10 title "Reference" with line
---
+++ src/testcases/source_term/source_term.py
@@ -0,0 +1,59 @@
+# Case Definition
+# Three functions read_subcases(), read_case_files() and read_subcase_files()
+# have to be implemented.
+
+import copy
+
+def build_subcase(source_active, mesh):
+	subcase = {}
+	subcase["solver_data"] = {}
+	subcase["jobscript_data"] = {}
+	
+	## Dictionaries for additional files
+	#subcase["subcase_analysis.py"] = {} 
+	#subcase["case_analysis.py"] = {} 
+	
+	if(source_active==1):
+	  subcase["solver_data"]["applySourceTerm"] = "true"
+	else:
+	  subcase["solver_data"]["applySourceTerm"] = "false"
+	
+	subcase["solver_data"]["numY"] = mesh
+	subcase["solver_data"]["numX"] = mesh*10
+	
+	return subcase
+	
+
+def build_subcase_name(source_active, mesh):
+	return str(source_active)+'/'+str(mesh)
+
+	
+def read_subcases():
+
+	# Initialize case_data
+	case_data = {}
+	
+	####################################################################
+	
+	####### Construct subcases via loops ########
+
+	## Arrays to loop over
+        sources= [0,1]
+        meshes = [25,50]
+        ##
+        
+        for source_active in sources:
+        	for mesh in meshes:
+        		subcase_name = build_subcase_name(source_active,mesh)
+        		case_data[subcase_name] = build_subcase(source_active,mesh)
+        		
+       #######################################################################
+      
+
+	return case_data
+
+def read_case_files():
+	return [] #["case_analysis.py"]
+
+def read_subcase_files():
+	return ["plot.gp"] #["subcase_analysis.py"]
