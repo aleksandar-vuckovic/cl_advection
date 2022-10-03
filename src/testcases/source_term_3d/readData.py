07e3b365da3917630638d84fd7155b1f706ce9be
Mathis Fricke <fricke@mma.tu-darmstadt.de>
2022-10-03 22:11:44 +0200

    Updated readData.py to collect max errors for different resolutions in one file.

--- src/testcases/source_term_3d/readData.py
+++ src/testcases/source_term_3d/readData.py
@@ -11,8 +11,11 @@ max_error_curvature = max(abs(curvData['curv']-curvData['curv_ref']))
 print("Maximum error curvature: "+str(max_error_curvature))
 # Write Max error to disk
 f = open("max_error_curvature.csv","w")
+g = open("../max_error_curvature.csv","a")
 f.write(str(dx)+","+str(max_error_curvature)+'\n')
+g.write(str(dx)+","+str(max_error_curvature)+'\n')
 f.close()
+g.close()
 
 
 # Read data
@@ -21,8 +24,11 @@ max_error_angle = max(abs(angleData['angle']-angleData['angle_ref']))
 print("Maximum error angle: "+str(max_error_angle))
 # Write Max error to disk
 f = open("max_error_contactAngle.csv","w")
+g = open("../max_error_contactAngle.csv","a")
 f.write(str(dx)+","+str(max_error_angle)+'\n')
+g.write(str(dx)+","+str(max_error_angle)+'\n')
 f.close()
+g.close()
 
 # Read data
 positionData = pd.read_csv('position.csv', usecols=[0,1,2], names=['time', 'position', 'position_ref'])
@@ -30,8 +36,11 @@ max_error_position = max(abs(positionData['position']-positionData['position_ref
 print("Maximum error position: "+str(max_error_position))
 # Write Max error to disk
 f = open("max_error_position.csv","w")
+g = open("../max_error_position.csv","a")
 f.write(str(dx)+","+str(max_error_position)+'\n')
+g.write(str(dx)+","+str(max_error_position)+'\n')
 f.close()
+g.close()
 
 # Read data
 gradientData = pd.read_csv('gradientNormAtContactPoint.csv', usecols=[0,1], names=['time', 'gradientNorm'])
@@ -39,5 +48,8 @@ max_gradient_deviation = max(abs(gradientData['gradientNorm']-1.0))
 print("Maximum gradient deviation: "+str(max_gradient_deviation))
 # Write Maximum deviation to disk
 f = open("max_gradient_deviation.csv","w")
+g = open("../max_gradient_deviation.csv","a")
 f.write(str(dx)+","+str(max_gradient_deviation)+'\n')
+g.write(str(dx)+","+str(max_gradient_deviation)+'\n')
 f.close()
+g.close()
