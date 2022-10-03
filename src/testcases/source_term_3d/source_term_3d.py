5dec9a94199e1a32a7864692082a81140be98c70
Mathis Fricke <fricke@mma.tu-darmstadt.de>
2022-10-03 16:22:23 +0200

    Started to setup 3d testcase with source term.

---
+++ src/testcases/source_term_3d/source_term_3d.py
@@ -0,0 +1,93 @@
+# Case Definition
+# Three functions read_subcases(), read_case_files() and read_subcase_files()
+# have to be implemented.
+
+import copy
+import math
+
+def build_subcase(source_active, mesh, setup, cfl):
+	subcase = {}
+	subcase["solver_data"] = {}
+	subcase["jobscript_data"] = {}
+	
+	## Dictionaries for additional files
+	#subcase["subcase_analysis.py"] = {} 
+	#subcase["case_analysis.py"] = {} 
+	
+	subcase["solver_data"]["cfl"] = cfl
+	
+	if(source_active==1):
+	  subcase["solver_data"]["applySourceTerm"] = "true"
+	else:
+	  subcase["solver_data"]["applySourceTerm"] = "false"
+	  
+	  
+	geometry1 =  """
+centerX=0.5
+centerY=-0.15
+centerZ=0.0
+radius=0.3
+
+expcpX=0.7598076211353315
+expcpY=0.0
+expcpZ=0.0
+expAngle=60.0""" 
+
+	subcase["solver_data"]["numY"] = mesh
+	subcase["solver_data"]["numX"] = mesh*2
+	subcase["solver_data"]["lenZ"] = 1.0/mesh
+	
+
+	## define the field
+	if(setup=="strawberry"):
+	
+		subcase["solver_data"]["time"]=1.0
+	 	subcase["solver_data"]["geometry"] = geometry1
+	
+		subcase["solver_data"]["field"] = """
+v0=-0.2
+c1=0.1
+c2=-2
+tau=0.4
+field=timeDependentNavierField"""
+
+	return subcase	
+	
+
+def build_subcase_name(source_active, mesh,setup,cfl):
+	return 'cfl_'+str(cfl)+'/'+setup+"/"+str(source_active)+'/'+str(mesh)
+
+	
+def read_subcases():
+
+	# Initialize case_data
+	case_data = {}
+	
+	####################################################################
+	
+	####### Construct subcases via loops ########
+
+	## Arrays to loop over
+	setups=["strawberry"]
+        sources= [0,1]
+        meshes = [50,100,200]
+        cfls = [0.5,1.0]
+        ##
+        
+        for source_active in sources:
+        	for mesh in meshes:
+        		for setup in setups:
+        			for cfl in cfls:
+        				subcase_name = build_subcase_name(source_active,mesh,setup,cfl)
+	        			case_data[subcase_name] = build_subcase(source_active,mesh,setup,cfl)
+        		
+       #######################################################################
+      
+
+	return case_data
+
+def read_case_files():
+	return [] #["case_analysis.py"]
+
+def read_subcase_files():
+	return ["plot.gp"] #["subcase_analysis.py"]
